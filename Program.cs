1241c5a15379cee2e39f16f2ef61468398ae07e0
froll_123@mail.ru <froll_123@mail.ru>
2022-12-26 15:04:29 +0300

    Added tasks to the 9th seminar on C#

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.Write("M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат, S = {SumNumbers(M, N)}");
+Console.ReadLine();
+ 
+int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                       
+    else if (M < N) return N + SumNumbers(M, N - 1); 
+    else return N + SumNumbers(M, N + 1);            
+}
+
+
+/*Задача 64: Написать рекурсивную функцию перевода из десятичной системы исчисления в двоичную.*/
+
+int Recurs(int number)  
+	{
+		if (number > 1)
+		{
+		Recurs(number / 2);
+		}
+		Console.Write(number % 2);
+		return number;
+	}
+
+        Console.Write("Введите натуральное число: ");
+		Console.Write("\n");
+		int number;
+		number = Convert.ToInt32(Console.ReadLine());
+		Recurs(number);
